ecdc0394a3eee3015c08ce6f3c0f5d09e9ab93ec
unknown <sawrivs@gmail.com>
2025-08-28 22:58:23 -0400

    Implement script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// Select all keys
+const keys = document.querySelectorAll('.key');
+
+// -- Listeners -- //
+// Add an event listener to all keys
+keys.forEach((key) => {
+  key.addEventListener('click', () => playNote(key));
+});
+
+// -- Handlers -- //
+function playNote(key) {
+  const noteAudio = document.getElementById(key.dataset.note);
+  noteAudio.currentTime = 0;
+  noteAudio.play();
+  key.classList.add('active');
+}
